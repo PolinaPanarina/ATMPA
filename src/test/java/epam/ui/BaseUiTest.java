9d718da67372ef8fcc39b44d289baea483924997
Polina_Panarina <polina_panarina@epam.com>
2024-06-05 15:36:44 +0300

    RP added

--- src/test/java/epam/ui/BaseUiTest.java
+++ src/test/java/epam/ui/BaseUiTest.java
@@ -1,6 +1,7 @@
 package epam.ui;
 
 import com.epam.driversettings.WebDriverFactory;
+import com.epam.reportportal.junit5.ReportPortalExtension;
 import com.epam.services.listener.TestListener;
 import com.epam.services.listener.TestNGListener;
 import org.junit.jupiter.api.AfterEach;
@@ -10,6 +11,7 @@ import org.testng.annotations.Listeners;
 
 @ExtendWith(TestListener.class)
 @Listeners(TestNGListener.class)
+@ExtendWith(ReportPortalExtension.class)
 public class BaseUiTest {
 
     @AfterEach
